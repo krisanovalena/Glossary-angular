851d6105b22148982714d7b4c34e5bdea26bd076
lenekke <lenekke@yandex.ru>
2021-11-16 19:58:52 +0300

    changes for deploy

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
-const http = require('http');
-const path = require('path');
-const compression = require('compression'); 
 const app = express();
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'dist/myapp')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/myapp/index.html'));
-});
-const port = process.env.PORT || 3000;
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log("running"));
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/myapp'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/myapp/'}),
+);
+
+app.listen(process.env.PORT || 8080);
