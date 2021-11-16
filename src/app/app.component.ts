3a48d5ec2bf368b1d8f07beb5f4fc1e9499b557f
lenekke <lenekke@yandex.ru>
2021-11-16 19:03:05 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+export interface ILetter {
+  title: string,
+  isSelect: boolean
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  letters1: ILetter[] = [
+    {title: 'А', isSelect: true},
+    {title: 'Б', isSelect: false},
+    {title: 'В', isSelect: false},
+    {title: 'Г', isSelect: false},
+    {title: 'Д', isSelect: false},
+    {title: 'Е', isSelect: false},
+    {title: 'Ж', isSelect: false},
+    {title: 'З', isSelect: false},
+    {title: 'И', isSelect: false},
+    {title: 'К', isSelect: false},
+    {title: 'Л', isSelect: false},
+    {title: 'М', isSelect: false},
+    {title: 'Н', isSelect: false},
+    {title: 'О', isSelect: false}
+  ];
+  letters2: ILetter[] = [
+    {title: 'П', isSelect: false},
+    {title: 'Р', isSelect: false},
+    {title: 'С', isSelect: false},
+    {title: 'Т', isSelect: false},
+    {title: 'У', isSelect: false},
+    {title: 'Ф', isSelect: false},
+    {title: 'Х', isSelect: false},
+    {title: 'Ц', isSelect: false},
+    {title: 'Ч', isSelect: false},
+    {title: 'Ш', isSelect: false},
+    {title: 'Щ', isSelect: false},
+    {title: 'Э', isSelect: false},
+    {title: 'Ю', isSelect: false},
+    {title: 'Я', isSelect: false},
+  ]
+}
